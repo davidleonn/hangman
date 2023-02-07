601106d8a01f642c2ff53f7568220a74274249aa
david <davidleon_06@hotmail.com>
2023-02-07 16:54:43 +0100

    components

--- my-handman/src/App.tsx
+++ my-handman/src/App.tsx
@@ -1,11 +1,38 @@
 import { useState } from "react";
 
 import words from "./data/wordList.json";
+import { HangmanDrawing } from "./components/HangmanDrawing";
+import { HangmanWord } from "./components/HangmanWord 2";
+import { Keyboard } from "./components/Keyboard";
+
+const randomWord = () => {
+  return words[Math.floor(Math.random() * words.length)];
+};
 
 function App() {
-  const [wordToGuess, setWordToGuess] = useState("Test");
+  const [wordToGuess, setWordToGuess] = useState(randomWord());
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
 
-  return <h1>hi</h1>;
+  console.log(wordToGuess);
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <div style={{ alignSelf: "stretch" }}>
+        <Keyboard />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ my-handman/src/components/HangmanDrawing.tsx
@@ -0,0 +1,133 @@
+const hangmanEstructure = (
+  <>
+    {" "}
+    <div
+      style={{
+        height: "50px",
+        width: "10px",
+        background: "black",
+        position: "absolute",
+        top: 0,
+        right: 0,
+      }}
+    />
+    <div
+      style={{
+        height: "10px",
+        width: "200px",
+        background: "black",
+        marginLeft: "120px",
+      }}
+    />
+    <div
+      style={{
+        height: "400px",
+        width: "10px",
+        background: "black",
+        marginLeft: "120px",
+      }}
+    />
+    <div
+      style={{
+        height: "10px",
+        width: "250px",
+        background: "black",
+      }}
+    />
+  </>
+);
+const head = (
+  <div
+    style={{
+      width: "50px",
+      height: "50px",
+      borderRadius: "100%",
+      border: "10px solid black",
+      position: "absolute",
+      top: "50px",
+      right: "-30px",
+    }}
+  />
+);
+const body = (
+  <div
+    style={{
+      width: "10px",
+      height: "100px",
+      background: "black",
+      position: "absolute",
+      top: "120px",
+      right: 0,
+    }}
+  />
+);
+const rightArm = (
+  <div
+    style={{
+      width: "70px",
+      height: "10px",
+      background: "black",
+      position: "absolute",
+      top: "150px",
+      right: "-70px",
+      rotate: "-30deg",
+      transformOrigin: "left bottom",
+    }}
+  />
+);
+const leftArm = (
+  <div
+    style={{
+      width: "70px",
+      height: "10px",
+      background: "black",
+      position: "absolute",
+      top: "150px",
+      right: "10px",
+      rotate: "30deg",
+      transformOrigin: "right bottom",
+    }}
+  />
+);
+const rightLeg = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      background: "black",
+      position: "absolute",
+      top: "210px",
+      right: "-90px",
+      rotate: "60deg",
+      transformOrigin: "left bottom",
+    }}
+  />
+);
+const leftLeg = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      background: "black",
+      position: "absolute",
+      top: "210px",
+      right: 0,
+      rotate: "-60deg",
+      transformOrigin: "right bottom",
+    }}
+  />
+);
+
+export const HangmanDrawing = () => {
+  return (
+    <div style={{ position: "relative" }}>
+      {head}
+      {body}
+      {rightArm}
+      {leftArm}
+      {rightLeg}
+      {leftLeg}
+      {hangmanEstructure}
+    </div>
+  );
+};
